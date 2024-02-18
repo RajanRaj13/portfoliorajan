cab554e59ffd3981d28cda56b6fd5df1476289ea
RajanRaj13 <swamidhyanraj@gmail.com>
2024-02-18 19:02:09 +0545

    first commit

---
+++ portfolio/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from portfolio import views
+
+urlpatterns = [
+    path('',views.home,name='home'),
+    path('about/',views.about,name='about'),
+    path('contact/',views.contact,name='contact'),
+    path('blog/',views.handleblog,name='handleblog'),
+    path('intershipdetails/',views.intershipdetails,name='intershipdetails'),
+    
+]
